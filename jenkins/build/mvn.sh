aa76922be204291dc4f06dabd6f9b192206a5810
AninditaMajumdar <43988527+AninditaMajumdar@users.noreply.github.com>
2019-07-17 07:53:39 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "****build jar ***"
-docker run --rm -it -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+WORKSPACE=/home/ubuntu/jenkins/jenkins_home/workspace/pipeline-docker-maven
+docker run --rm -it -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
