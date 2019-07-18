a1e35fb0df795a848d19101942f37bb04ce3a9b6
AninditaMajumdar <43988527+AninditaMajumdar@users.noreply.github.com>
2019-07-18 11:49:48 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,5 +3,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 scp -i ~/.ssh/id_rsa /tmp/.auth anindita@34.242.80.140:/tmp/.auth
+scp -i ~/.ssh/id_rsa ./jenkins/deploy/publish.sh anindita@34.242.80.140:/tmp/publish.sh
+ssh -i ~/.ssh/id_rsa anindita@34.242.80.140 "/tmp/publish.sh"
 
 
