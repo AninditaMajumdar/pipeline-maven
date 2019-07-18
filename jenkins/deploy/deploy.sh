1fae61dfd762ba90e623b09bd173f2d64079db5c
AninditaMajumdar <43988527+AninditaMajumdar@users.noreply.github.com>
2019-07-18 10:31:52 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,4 +2,5 @@
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
-ssh -i /opt/prod anindita@34.242.80.140
+scp -i /opt/prod /tmp/.auth anindita@34.242.80.140:/tmp/.auth
+
